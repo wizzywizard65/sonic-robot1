6304f4c06c94ce743335db3c1994ae922bd1566a
Gordy Campbell <gordy2007@hotmail.co.uk>
2026-03-12 16:57:10 +0000

    revert test

--- build_files/build.sh
+++ build_files/build.sh
@@ -58,8 +58,7 @@ dnf install -y --allowerasing \
  libva-intel-driver \
  gnome-shell-extension-dash-to-dock \
  gnome-shell-extension-caffeine \
- gnome-shell-extension-appindicator \
- htop
+ gnome-shell-extension-appindicator
 
 #remove firefox
 
