be41ffe8eba680304f7839d3a7b921c95e79b2f8
Gordy Campbell <gordy2007@hotmail.co.uk>
2026-03-23 10:52:23 +0000

    reverting

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,6 +33,10 @@ dnf install -y --allowerasing \
  cockpit-system \
  cockpit-ws \
  cockpit-ws-selinux \
+ docker-buildx-plugin \
+ docker-ce docker-ce-cli \
+ docker-compose-plugin \
+ docker-model-plugin \
  podman-machine \
  podman-compose \
  podman-tui \
@@ -82,5 +86,5 @@ dnf -y copr disable ublue-os/packages
 
 systemctl enable podman.socket
 systemctl enable cockpit.socket
-#systemctl enable docker.socket
+systemctl enable docker.socket
 systemctl enable libvirtd
